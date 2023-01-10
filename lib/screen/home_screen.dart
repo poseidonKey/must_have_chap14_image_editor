0ab3a9cfb75d5a3616d3d9e088d39955db81b072
poseidon <61605433+poseidonKey@users.noreply.github.com>
2023-01-10 15:18:04 +0900

    my completed

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,17 +1,140 @@
+import 'dart:io';
+import 'dart:ui' as ui;
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:must_have_chap14_image_editor/component/emoticon_sticker.dart';
+import 'package:must_have_chap14_image_editor/component/footer.dart';
+import 'package:must_have_chap14_image_editor/component/main_app_bart.dart';
+import 'package:uuid/uuid.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../model/sticker_model.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? image; // 선택한 이미지를 저장할 변수
+  Set<StickerModel> stickers = {}; // 화면에 추가된 스티커를 저장할 변수
+  String? selectedId; // 현재 선택된 스티커의 ID
+  GlobalKey imgKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Material App Bar'),
-      ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          renderBody(),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: MainAppbar(
+              onPickImage: onPickImage,
+              onSaveImage: onSaveImage,
+              onDeleteImage: onDeleteImage,
+            ),
+          ),
+          if (image != null)
+            Positioned(
+              left: 0,
+              bottom: 0,
+              right: 0,
+              child: Footer(onEmoticonTap: onEmoticonTap),
+            ),
+        ],
       ),
     );
+  }
+
+  void onEmoticonTap(int index) async {
+    setState(() {
+      stickers = {
+        ...stickers,
+        StickerModel(
+            id: const Uuid().v4(), imgPath: "asset/img/emoticon_$index.png")
+      };
+    });
+  }
+
+  Widget renderBody() {
+    if (image != null) {
+      return RepaintBoundary(
+        key: imgKey,
+        child: Positioned.fill(
+          child: InteractiveViewer(
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Image.file(
+                  File(image!.path),
+                  fit: BoxFit.cover,
+                ),
+                ...stickers.map(
+                  (sticker) => Center(
+                    child: EmoticonSticker(
+                      key: ObjectKey(sticker.id),
+                      onTransform: () {
+                        onTransform(sticker.id);
+                      },
+                      imgPath: sticker.imgPath,
+                      isSelected: selectedId == sticker.id,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    } else {
+      // ➋ 이미지 선택이 안 된 경우 이미지 선택 버튼 표시
+      return Center(
+        child: TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.grey,
+          ),
+          onPressed: onPickImage,
+          child: const Text('이미지 선택하기'),
+        ),
+      );
+    }
+  }
+
+  void onPickImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      this.image = image;
+    });
+  }
+
+  void onSaveImage() async {
+    RenderRepaintBoundary boundary =
+        imgKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage();
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    Uint8List pngBytes = byteData!.buffer.asUint8List();
+    await ImageGallerySaver.saveImage(pngBytes, quality: 100);
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text("save Complted")),
+    );
+  }
+
+  void onDeleteImage() async {
+    setState(() {
+      stickers = stickers.where((sticker) => sticker.id != selectedId).toSet();
+    });
+  }
+
+  void onTransform(String id) {
+    setState(() {
+      selectedId = id;
+    });
   }
 }
